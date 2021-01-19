738c08ea751cbcb05f0a3bc452ba29f65500d5e9
emre şener <emre.sener33@gmail.com>
2021-01-19 14:34:44 +0300

    Add project files.

---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer()
+            {
+                Id = 1,
+                name = "Emre",
+                LastName = "Şener",
+
+            };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(customer);
+            musteriManager.list(customer);
+            musteriManager.Sil(customer);
+        }
+    }
+
+
+    class Customer
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string LastName { get; set; }
+
+
+
+    }
+
+    class MusteriManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(" ( " + customer.Id + " - " + customer.name + " " + customer.LastName + " ) " + " müşteri eklendi");
+        }
+
+        public void list(Customer customer)
+        {
+            Console.WriteLine(customer.name + " " + customer.LastName + " Listelendi.");
+        }
+
+        public void Sil(Customer customer)
+        {
+            Console.WriteLine(customer.name + " " + customer.LastName + " Silimdi.");
+        }
+
+    }
+
+}
